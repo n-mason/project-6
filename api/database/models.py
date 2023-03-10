434a4ce307a9b21218a27706bc3eb43e79ffc910
Nathaniel Mason <nmason0204@gmail.com>
2023-03-09 20:13:26 -0800

    small edit to comments

--- api/database/models.py
+++ api/database/models.py
@@ -4,7 +4,7 @@ import datetime
 class Checkpoint(EmbeddedDocument):
     """
     A MongoEngine EmbeddedDocument containing:
-    distance: MongoEngine float field, required, (checkpoint distance in kilometers),
+    distance: MongoEngine string field, required, (checkpoint distance in kilometers),
 		location: MongoEngine string field, optional, (checkpoint location name),
 		open_time: MongoEngine string field, required, (checkpoint opening time),
 		close_time: MongoEngine string field, required, (checkpoint closing time).
@@ -18,7 +18,7 @@ class Checkpoint(EmbeddedDocument):
 class BrevetClass(Document):
     """
     A MongoEngine document containing:
-		length: MongoEngine string field, required
+		length: MongoEngine float field, required
 		start_time: MongoEngine string field, required
 		checkpoints: MongoEngine list field of Checkpoints, required
     """
